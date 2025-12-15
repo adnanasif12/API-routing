b73f9b5e03d2d42311e704fcffdb87a30deb0aca
unknown <ashibadnan42.com>
2025-12-15 09:14:14 +0600

    added here post request

--- user.js
+++ user.js
@@ -2,6 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 
+class User {
+  constructor({name, age, address,location}){
+    this.age = age;
+    this.name = name;
+    this.address = address;
+    this.location = location
+  }
+}
+
 router.get('/users/:id', (req, res) => {
   const userID = req.params.id;
   const filter = req.query.filter;
@@ -13,31 +22,21 @@ router.get('/users/:id', (req, res) => {
   res.send(`User ID is ${userID} and filter is ${filter}`);
 });
 
-
-
-
 router.post('/users', (req, res) => {
-  const{name, age}= req.body;
-
+  // const{name, age}= req.body;
   // if(!name || !age){
   //   console.log('Invalid data');
   // }
 
+  const user = new User(req.body);
 
-  if(!name || !age) {
-    return res.status(400).json({error: "Name is required"});
-  }
-
-
-  const user = {name, age};
+  // if(!name || !age) {
+  //   return res.status(400).json({error: "Name is required"});
+  // }
+  // const user = {name, age};
   res.status(201).json({
     message: "User created successfully",
     user: user
     })   
-
-
-
 });
-
-
 module.exports = router;
